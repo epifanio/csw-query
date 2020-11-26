b2b497031e32cfb43743c373cb5264c3c2d9f4dc
epifanio <epiesasha@me.com>
2020-11-26 15:44:39 +0100

    Create csw_search.py

---
+++ csw_search.py
@@ -0,0 +1,73 @@
+from owslib import fes
+
+
+def fes_date_filter(start, stop, constraint="overlaps"):
+    """
+    Take datetime-like objects and returns a fes filter for date range
+    (begin and end inclusive).
+    NOTE: Truncates the minutes!!!
+
+    Examples
+    --------
+    >>> from datetime import datetime, timedelta
+    >>> stop = datetime(2010, 1, 1, 12, 30, 59).replace(tzinfo=pytz.utc)
+    >>> start = stop - timedelta(days=7)
+    >>> begin, end = fes_date_filter(start, stop, constraint='overlaps')
+    >>> begin.literal, end.literal
+    ('2010-01-01 12:00', '2009-12-25 12:00')
+    >>> begin.propertyoperator, end.propertyoperator
+    ('ogc:PropertyIsLessThanOrEqualTo', 'ogc:PropertyIsGreaterThanOrEqualTo')
+    >>> begin, end = fes_date_filter(start, stop, constraint='within')
+    >>> begin.literal, end.literal
+    ('2009-12-25 12:00', '2010-01-01 12:00')
+    >>> begin.propertyoperator, end.propertyoperator
+    ('ogc:PropertyIsGreaterThanOrEqualTo', 'ogc:PropertyIsLessThanOrEqualTo')
+
+    """
+    start = start.strftime("%Y-%m-%d %H:00")
+    stop = stop.strftime("%Y-%m-%d %H:00")
+    if constraint == "overlaps":
+        propertyname = "apiso:TempExtent_begin"
+        begin = fes.PropertyIsLessThanOrEqualTo(propertyname=propertyname, literal=stop)
+        propertyname = "apiso:TempExtent_end"
+        end = fes.PropertyIsGreaterThanOrEqualTo(
+            propertyname=propertyname, literal=start
+        )
+    elif constraint == "within":
+        propertyname = "apiso:TempExtent_begin"
+        begin = fes.PropertyIsGreaterThanOrEqualTo(
+            propertyname=propertyname, literal=start
+        )
+        propertyname = "apiso:TempExtent_end"
+        end = fes.PropertyIsLessThanOrEqualTo(propertyname=propertyname, literal=stop)
+    else:
+        raise NameError("Unrecognized constraint {}".format(constraint))
+    return begin, end
+
+
+def get_csw_records(csw, filter_list, pagesize=10, maxrecords=1000):
+    """Iterate `maxrecords`/`pagesize` times until the requested value in
+    `maxrecords` is reached.
+    """
+    from owslib.fes import SortBy, SortProperty
+
+    # Iterate over sorted results.
+    sortby = SortBy([SortProperty("dc:title", "ASC")])
+    csw_records = {}
+    startposition = 0
+    nextrecord = getattr(csw, "results", 1)
+    while nextrecord != 0:
+        csw.getrecords2(
+            constraints=filter_list,
+            startposition=startposition,
+            maxrecords=pagesize,
+            sortby=sortby,
+        )
+        csw_records.update(csw.records)
+        if csw.results["nextrecord"] == 0:
+            break
+        startposition += pagesize + 1  # Last one is included.
+        if startposition >= maxrecords:
+            break
+    csw.records.update(csw_records)
+    
